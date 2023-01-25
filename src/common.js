feac412524e865dfdbdd643b165a2b287df655e2
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-01-25 14:05:29 +0530

    added tooltip

--- src/common.js
+++ src/common.js
@@ -14,26 +14,85 @@ var svg = d3.select("#chart")
 var g = svg.append("g")
             .attr("transform", "translate ("+ width / 2 + "," + height / 2 + ")");
 
-var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
+var color = d3.scaleOrdinal()                
+                .range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
 
 var pie = d3.pie().value((d) => {return d.share})
-var arc = d3.arc()            
-            .innerRadius(0)
-            .outerRadius(radius)
 
-console.log(pie(data))
+var tooltip = d3.select("body")
+                .append("div")
+                .attr('class', 'tooltip')
+                .style("position", "absolute")
+                .style("z-index", "10")
+                .style("visibility", "hidden")
+                .style("background", "#fff")
+                .style("padding", "5px")
+                .style("border-radius", "7px")
+                .text("");
+        
 
-var arcs = g.selectAll("arc")
+var arc = g.selectAll("arc")
             .data(pie(data))
             .enter()
-            /* .append("g")
-            .attr("class", "arc") */
 
-    arcs.append("path")
-        .attr("class","tooltip_area")
-        .attr("fill", (d,i) => {
-            return color(i)
-        })
-        .attr("d", arc);
+var path = d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius);
 
+    arc.append("path")        
+        .attr("d", path)        
+        .attr("fill", (d) => {return color(d.data.name)})
+
+        // mouseover effects
+        .on("mouseover", (d) => { 
+                                    tooltip.text(d.data.share); 
+                                    return tooltip.style("visibility", "visible");
+                                })
+
+        .on("mousemove", () => { 
+                                    return tooltip.style("top", (d3.event.pageY-10)+"px")
+                                                  .style("left",(d3.event.pageX+15)+"px");
+                            })
+
+        .on("mouseout", () => { return tooltip.style("visibility", "hidden") })
+
+
+var label = d3.arc()
+                .innerRadius(0)
+                .outerRadius(radius)
+                
+            arc.append("text")
+                .attr("transform", (d) => `translate(${label.centroid(d)})`)
+                .text((d) => { return d.data.name })                
+                .style("font-family", "arial")
+                .style("font-size", "15")
+                
+
+// var mouseover = 
+// console.log(pie(data))
+
+
+/* var arcs = g.selectAll("arc")
+            .data(pie(data))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+            .append("path")
+            .attr("class","tooltip_area")
+            .attr("fill", (d,i) => {
+                return color(i)
+            })
+            .attr("d", arc); */
+            
+
+    
+
+/* svg.selectAll(".arc")
+    .data(pie(data))
+    .enter()
+    .append("text")
+    .text((d) => { return d.data.name })
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";  })
+    .style("text-anchor", "middle")
+    .style("font-size", 17) */
 
